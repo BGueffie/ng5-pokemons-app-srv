46d3346c866d87dfee8f3f9f58ea96b835835237
BGueffier <benjamin.gueffier@operantic.com>
2020-11-09 14:57:16 +0100

    salt password bcrypt

--- auth/models/user.js
+++ auth/models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
-    createdOn: {type : Date,default:Date.now}
+    createdOn:{type: Date , default: Date.now}
 });
 
 module.exports = mongoose.model('User',userSchema);
--- auth/routes/index.js
+++ auth/routes/index.js
@@ -2,35 +2,53 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
 
+
+var BCRYPT_SALT_ROUNDS = 12;
 router.post('/register',(req,res) => {
-    console.log('user from req.body >>>', req.body);
     const newUser = new User(req.body);
-    newUser.save((err,user) => {
-        if(err) return res.status(500).json(err);
-        req.login(req.body, (err) => {
-            if (err) console.log('err in register | req.login()',err);
-            res.status(201).json(user);
-        });
-    });
+    bcrypt.hash(newUser.password, BCRYPT_SALT_ROUNDS)
+          .then((hashedPassword) => {
+              newUser.password = hashedPassword
+              return newUser.save((err,user) => {
+                  if (err) return res.status(500).json(err);
+                  req.login(req.body, (err) => {
+                      if(err) console.log('err in register | req.login()',err);
+                      res.status(201).json(user);
+                  });
+              });
+          });
 });
 
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/auth/sucess',
-    failureRedirect: '/auth/failure'
-}));
+router.post('/login', async (req,res) => {
+    try {
+        var user = await User.findOne({ username : req.body.username }).exec();
+        if(!user) {
+            return res.status(400).send({ msg: "The username does not exist" });
+        }
+        if(bcrypt.compareSync(req.body.password,user.password)) {
+            return res.status(200).send({msg: "You have logged in"});
+        }
+        res.status(400).send({ msg: "The password is invalid" });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+        
 
-router.get('/sucess', (req,res) => {
-    res.status(200).json( { msg: 'logged in !', user: req.user});
 });
 
-router.get('/failure',(req,res) => {
-    res.status(401).json( { msg: 'NOT logged in !'});
-});
 
 router.get('/logout', (req,res) => {
     req.logOut();
     res.status(200).json({ msg: 'logged out succesfully'});
-})
+});
 
+router.get('/users', (req,res) => {
+    User.find()
+        .exec()
+        .then(users => res.status(200).json(users))
+        .catch(err => res.status(500).json({ msg : "Users not found", error : err}));
+
+});
 module.exports = router;
